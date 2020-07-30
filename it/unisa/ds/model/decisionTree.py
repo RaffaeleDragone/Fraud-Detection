ca59553c892ab04b3a2050c4ac82996d45f5367a
raffaeledragone <raffidragone@gmail.com>
2020-07-30 11:14:27 +0200

    Decision Tree and Random Forest implementation

--- it/unisa/ds/model/decisionTree.py
+++ it/unisa/ds/model/decisionTree.py
@@ -1,5 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn import metrics
 import numpy as np
@@ -13,44 +14,59 @@ dataFrameFull = du.dataCleaningAndEngineering(dataFrame)
 dataFrameWithoutLabel = dataFrameFull[0]
 label = dataFrameFull[3]
 
-X_train, X_test, y_train, y_test = train_test_split(dataFrameWithoutLabel, label, test_size=0.2, random_state=0, stratify= label)
-'''
-def accuracy_dectree(depth):
+X_train, X_test, y_train, y_test = train_test_split(dataFrameWithoutLabel, label, test_size=0.2, random_state=42, stratify= label)
+
+print('Transazioni Fraudolente TRAINING SET ', len(y_train.loc[(y_train==1)]))
+print('Transazioni Fraudolente TEST SET ', len(y_test.loc[(y_test==1)]))
+
+print('Transazioni NON Fraudolente TRAIN SET ', len(y_train.loc[(y_train==0)]))
+print('Transazioni NON Fraudolente TEST SET ', len(y_test.loc[(y_test==0)]))
+
+def decision_tree_model(depth):
     # Build and train model
     # criterion = "gini | default " - entropy ".
     # splitter = "best | default " - "random".
     # max_depth = "None | default " -
     model = DecisionTreeClassifier(max_depth=depth)
 
-    randomForest = RandomForestClassifier(max_depth=depth)
-
     model.fit(X_train, y_train)
-
-    randomForest.fit(X_train,y_train)
-
 
     # Apply model to validation data
     y_predict = model.predict(X_test)
-    y_predictForest = randomForest.predict(X_test)
 
     # Accuracy
-    #print('Accuracy Score:', metrics.accuracy_score(y_test, y_predict))
-    print('Accuracy Score Decision Tree:', metrics.classification_report(y_test, y_predict))
+    print('Accuracy Score:', metrics.accuracy_score(y_test, y_predict))
+    print('Accuracy Score Decision Tree: \n', metrics.classification_report(y_test, y_predict))
 
-    print('Accuracy Score Random Forest:', metrics.classification_report(y_test, y_predictForest)) 
     from sklearn.metrics import confusion_matrix
     confusion_matx = confusion_matrix(y_test, y_predict)
     print(confusion_matx)
 
-    file_name='result_graph_depth'+str(i)+'.dot'
-    export_graphviz(model, out_file=file_name, class_names=True,
+    export_graphviz(model, out_file='classifier_graph.dot', class_names=True,
                     feature_names=dataFrameWithoutLabel.columns, rounded=True, filled=True)
-    '''
+
+def random_forest_model(depth,n_ests):
+
+    randomForest = RandomForestClassifier(max_depth=depth)
+
+    randomForest.fit(X_train,y_train)
+
+    # Apply model to validation data
+    y_predictForest = randomForest.predict(X_test)
+
+    # Accuracy
+    print('Accuracy Score Random Forest: \n ', metrics.classification_report(y_test, y_predictForest))
+    print('Accuracy Score:', metrics.accuracy_score(y_test, y_predictForest))
+    from sklearn.metrics import confusion_matrix
+    confusion_matx = confusion_matrix(y_test, y_predictForest)
+    print(confusion_matx)
+
+
 #for i in range(1,15):
     #print('********************** Decision Tree depth : ',i,'**************************\n')
     #accuracy_dectree(i)
 
-#accuracy_dectree(10)
+
 
 
 # function for fitting trees of various depths on the training data using cross-validation
@@ -87,65 +103,69 @@ def plot_cross_validation_on_trees(depths, cv_scores_mean, cv_scores_std, accura
     ax.legend()
     plt.show()
 
+def cross_validation_split(type_model):
+    #type_model = 0 : decision tree | 1 : random forest
+    kf = KFold(n_splits=10, random_state=42, shuffle=True)
+    idx = type_model
 
+    precision_values=[]
+    recall_values=[]
+    f1_values=[]
+    accuracy_values=[]
+
+    for train_index, test_index in kf.split(dataFrameWithoutLabel):
+
+
+        data_train = dataFrameWithoutLabel.iloc[train_index]
+
+        target_train = label.iloc[train_index]
+
+        data_test = dataFrameWithoutLabel.iloc[test_index]
+        target_test = label.iloc[test_index]
+
+        if(type_model==0):
+            model = DecisionTreeClassifier(max_depth=5)
+
+            model.fit(data_train, target_train)
+
+            # Apply model to validation data
+            y_predict = model.predict(data_test)
+
+            accuracy_values.append(metrics.accuracy_score(target_test, y_predict))
+
+            precision_values.append(metrics.precision_score(target_test, y_predict))
+
+            recall_values.append(metrics.recall_score(target_test, y_predict))
+
+            f1_values.append(metrics.f1_score(target_test, y_predict))
+
+            # Accuracy
+            print('Accuracy Score:', metrics.accuracy_score(target_test, y_predict))
+
+
+
+
+            print('Accuracy Score Decision Tree:', metrics.classification_report(target_test, y_predict))
+
+            from sklearn.metrics import confusion_matrix
+            confusion_matx = confusion_matrix(target_test, y_predict)
+            print(confusion_matx)
+
+            export_graphviz(model, out_file='classifier_graph'+str(idx+1)+'.dot', class_names=True,
+                            feature_names=dataFrameWithoutLabel.columns, rounded=True, filled=True)
+
+
+    print("Media Accuratezza : ",np.mean(accuracy_values))
+    print("Media Precision : ", np.mean(precision_values))
+    print("Media Recall : ", np.mean(recall_values))
+    print("Media F1 Score : ", np.mean(f1_values))
+
+
+
+decision_tree_model(5)
+#cross_validation_split(0)
+random_forest_model(5,50)
 # fitting trees of depth 1 to 24
-sm_tree_depths = range(1, 20)
-sm_cv_scores_mean, sm_cv_scores_std, sm_accuracy_scores = run_cross_validation_on_trees(X_train, y_train,
-                                                                                        sm_tree_depths)
-
-# plotting accuracy
-plot_cross_validation_on_trees(sm_tree_depths, sm_cv_scores_mean, sm_cv_scores_std, sm_accuracy_scores,
-                               'Accuracy per decision tree depth on training data')
-
-idx_max = sm_cv_scores_mean.argmax()
-sm_best_tree_depth = sm_tree_depths[idx_max]
-sm_best_tree_cv_score = sm_cv_scores_mean[idx_max]
-sm_best_tree_cv_score_std = sm_cv_scores_std[idx_max]
-print('The depth-{} tree achieves the best mean cross-validation accuracy {} +/- {}% on training dataset'.format(
-      sm_best_tree_depth, round(sm_best_tree_cv_score*100,5), round(sm_best_tree_cv_score_std*100, 5)))
-
-#Confusion Matrix
-
-#from sklearn.metrics import confusion_matrix
-#cm = confusion_matrix(y_test, y_predict)
-#print(cm)
-
-#export_graphviz(model, out_file='classifier_graph.dot', class_names=True,feature_names=dataFrameWithoutLabel.columns, rounded=True, filled=True)
-
-'''
-# Compare actual and predicted values
-actual_vs_predict = pd.DataFrame({'Actual': y_val,
-                                'Prediction': y_predict})
-print(actual_vs_predict.sample(12))
-
-
-prob= model.predict_proba([[1,1,181.0,181.0,0.0,21182.0,0.0]])
-print("Prob: {}".format(prob))
-
-pred= model.predict([[1,1,181.0,181.0,0.0,21182.0,0.0]])
-print("Pred: {}".format(pred))
-
-# Evaluate model
-print('Classification metrics: \n', classification_report(y_val, y_predict))
-
-dot_data = export_graphviz(model, out_file=None, class_names=True,feature_names=dataFrameWithoutLabel.columns, rounded=True, filled=True)
-
-graph = pydotplus.graph_from_dot_data(dot_data)
-#display(Image(data=graph.create_pdf()))
-display(data=graph.create_pdf())
-'''
-'''
-tree_clf= DecisionTreeClassifier(max_depth=2,random_state=42)
-tree_clf.fit(dataFrameWithoutLabel, label)
-
-export_graphviz(tree_clf, out_file="decisiontree.dot", class_names=True,feature_names=dataFrameWithoutLabel.columns, rounded=True, filled=True)
-path = 'decisiontree.dot'
-s = Source.from_file(path)
-s.view()
-
-prob= tree_clf.predict_proba([[1,1,181.0,181.0,0.0,21182.0,0.0]])
-print("Prob: {}".format(prob))
-
-pred= tree_clf.predict([[1,1,181.0,181.0,0.0,21182.0,0.0]])
-print("Pred: {}".format(pred))
-'''
+#sm_tree_depths = range(1, 20)
+#sm_cv_scores_mean, sm_cv_scores_std, sm_accuracy_scores = run_cross_validation_on_trees(X_train, y_train,
+#                                                                                        sm_tree_depths)
