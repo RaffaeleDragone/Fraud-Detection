72a9027860961b6a9b45408a8dd206e2792d151f
raffaeledragone <raffidragone@gmail.com>
2020-08-12 17:08:01 +0200

    Final Model

--- it/unisa/ds/model/decisionTree.py
+++ it/unisa/ds/model/decisionTree.py
@@ -41,9 +41,49 @@ def decision_tree_model(depth):
     
     confusion_matx = confusion_matrix(y_test, y_predict)
     print(confusion_matx)
+    class_names = [0, 1]
+    plt.figure()
+    plot_confusion_matrix(confusion_matx
+                          , classes=class_names
+                          , title='Confusion matrix')
+    plt.show()
 
     export_graphviz(model, out_file='classifier_graph.dot', class_names=True,
                     feature_names=dataFrameWithoutLabel.columns, rounded=True, filled=True)
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    import itertools
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=0)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        #print("Normalized confusion matrix")
+    else:
+        1#print('Confusion matrix, without normalization')
+
+    #print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 def random_forest_model(depth,n_ests):
 
@@ -84,6 +124,9 @@ def run_cross_validation_on_trees(X, y, tree_depths, cv=5, scoring='accuracy'):
     cv_scores_mean = np.array(cv_scores_mean)
     cv_scores_std = np.array(cv_scores_std)
     accuracy_scores = np.array(accuracy_scores)
+
+
+
     return cv_scores_mean, cv_scores_std, accuracy_scores
 
 def run_cross_validation_on_random_forest(X, y, tree_depths, cv=5, scoring='accuracy'):
@@ -103,6 +146,15 @@ def run_cross_validation_on_random_forest(X, y, tree_depths, cv=5, scoring='accu
     cv_scores_mean = np.array(cv_scores_mean)
     cv_scores_std = np.array(cv_scores_std)
     accuracy_scores = np.array(accuracy_scores)
+
+    idx_max = sm_cv_scores_mean.argmax()
+    sm_best_tree_depth = sm_tree_depths[idx_max]
+    sm_best_tree_cv_score = sm_cv_scores_mean[idx_max]
+    sm_best_tree_cv_score_std = sm_cv_scores_std[idx_max]
+    print('The depth-{} tree achieves the best mean cross-validation accuracy {} +/- {}% on training dataset'.format(
+        sm_best_tree_depth, round(sm_best_tree_cv_score * 100, 5), round(sm_best_tree_cv_score_std * 100, 5)))
+
+
     return cv_scores_mean, cv_scores_std, accuracy_scores
 
 # function for plotting cross-validation results
@@ -181,12 +233,19 @@ def cross_validation_split(type_model):
 
 #decision_tree_model(7)
 #cross_validation_split(0)
-random_forest_model(7,50)
+#random_forest_model(7,50)
 # fitting trees of depth 1 to 24
-#sm_tree_depths = range(1, 20)
-#sm_cv_scores_mean, sm_cv_scores_std, sm_accuracy_scores = run_cross_validation_on_trees(X_train, y_train,
-#                                                                                        sm_tree_depths)
+sm_tree_depths = range(6, 10)
+sm_cv_scores_mean, sm_cv_scores_std, sm_accuracy_scores = run_cross_validation_on_trees(X_train, y_train,
+                                                                                        sm_tree_depths)
+plot_cross_validation_on_trees(sm_tree_depths, sm_cv_scores_mean, sm_cv_scores_std, sm_accuracy_scores, "Cross Validation Decision Tree")
 
+idx_max = sm_cv_scores_mean.argmax()
+sm_best_tree_depth = sm_tree_depths[idx_max]
+sm_best_tree_cv_score = sm_cv_scores_mean[idx_max]
+sm_best_tree_cv_score_std = sm_cv_scores_std[idx_max]
+print('The depth-{} tree achieves the best mean cross-validation accuracy {} +/- {}% on training dataset'.format(
+        sm_best_tree_depth, round(sm_best_tree_cv_score * 100, 5), round(sm_best_tree_cv_score_std * 100, 5)))
 #sm_tree_depths = range(5, 10)
 #sm_cv_scores_mean, sm_cv_scores_std, sm_accuracy_scores = run_cross_validation_on_random_forest(X_train, y_train,sm_tree_depths)
 #plot_cross_validation_on_trees(sm_tree_depths, sm_cv_scores_mean, sm_cv_scores_std, sm_accuracy_scores, "Cross Validation Random Forest")
