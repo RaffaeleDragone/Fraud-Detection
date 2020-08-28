0b39380ce90c59167c2ee0e659dbef6425827ec8
raffaeledragone <raffidragone@gmail.com>
2020-08-28 10:27:31 +0200

    Comparative analisys random forest

--- it/unisa/ds/test/comparative_analysis.py
+++ it/unisa/ds/test/comparative_analysis.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix
 from it.unisa.ds.test import util_cleaning as util
@@ -35,13 +36,27 @@ def comparing_step1(df):
     X = simpleDf[0]
     Y = simpleDf[1]
     decision_tree_model(7,X,Y)
-
     # Step1 : dataFrame con soltanto CASH_OUT e TRASFER
     dataFrameStep1 = util.step_1(df)
 
     X2 = dataFrameStep1[0]
     Y2 = dataFrameStep1[1]
     decision_tree_model(7, X2, Y2)
+
+def comparing_step1_randomforest(df):
+
+    #dataFrame vergine
+    simpleDf = util.simple_df(df)
+
+    X = simpleDf[0]
+    Y = simpleDf[1]
+    random_forest_model(X,Y)
+    # Step1 : dataFrame con soltanto CASH_OUT e TRASFER
+    dataFrameStep1 = util.step_1(df)
+
+    X2 = dataFrameStep1[0]
+    Y2 = dataFrameStep1[1]
+    random_forest_model(X2,Y2)
 
 def comparing_step2(df):
 
@@ -59,6 +74,22 @@ def comparing_step2(df):
     Y2 = dataFrameStep2[1]
     decision_tree_model(7, X2, Y2)
 
+def comparing_step2_randomforest(df):
+
+    # Step1 : dataFrame con soltanto CASH_OUT e TRASFER
+    dataFrameStep1 = util.step_1(df)
+
+    X = dataFrameStep1[0]
+    Y = dataFrameStep1[1]
+    random_forest_model(X,Y)
+
+    # Step2 : dataFrame con soltanto CASH_OUT e TRASFER - IsFlaggedFraud
+    dataFrameStep2 = util.step_2(df)
+
+    X2 = dataFrameStep2[0]
+    Y2 = dataFrameStep2[1]
+    random_forest_model(X2, Y2)
+
 def comparing_step3(df):
 
     # Step1 : dataFrame con soltanto CASH_OUT e TRASFER
@@ -74,6 +105,23 @@ def comparing_step3(df):
     X2 = dataFrameStep3[0]
     Y2 = dataFrameStep3[1]
     decision_tree_model(7, X2, Y2)
+
+def comparing_step3_randomforest(df):
+
+    # Step1 : dataFrame con soltanto CASH_OUT e TRASFER
+    dataFrameStep2 = util.step_2(df)
+
+    X = dataFrameStep2[0]
+    Y = dataFrameStep2[1]
+    random_forest_model( X, Y)
+
+    # Step2 : dataFrame con soltanto CASH_OUT e TRASFER - IsFlaggedFraud
+    dataFrameStep3 = util.step_3(df)
+
+    X2 = dataFrameStep3[0]
+    Y2 = dataFrameStep3[1]
+    random_forest_model( X2, Y2)
+
 
 def comparing_step4(df):
 
@@ -91,6 +139,22 @@ def comparing_step4(df):
     Y2 = dataFrameStep4[1]
     decision_tree_model(7, X2, Y2)
 
+def comparing_step4_randomforest(df):
+
+    # Step1 : dataFrame con soltanto CASH_OUT e TRASFER
+    dataFrameStep3 = util.step_3(df)
+
+    X = dataFrameStep3[0]
+    Y = dataFrameStep3[1]
+    random_forest_model(X, Y)
+
+    # Step2 : dataFrame con soltanto CASH_OUT e TRASFER - IsFlaggedFraud
+    dataFrameStep4 = util.step_4(df)
+
+    X2 = dataFrameStep4[0]
+    Y2 = dataFrameStep4[1]
+    random_forest_model(X2, Y2)
+
 def comparing_step5(df):
         # Step1 : dataFrame con soltanto CASH_OUT e TRASFER
         dataFrameStep4 = util.step_4(df)
@@ -105,6 +169,22 @@ def comparing_step5(df):
         X2 = dataFrameStep5[0]
         Y2 = dataFrameStep5[1]
         decision_tree_model(7, X2, Y2)
+
+
+def comparing_step5_randomforest(df):
+    # Step1 : dataFrame con soltanto CASH_OUT e TRASFER
+    dataFrameStep4 = util.step_4(df)
+
+    X = dataFrameStep4[0]
+    Y = dataFrameStep4[1]
+    random_forest_model(X, Y)
+
+    # Step2 : dataFrame con soltanto CASH_OUT e TRASFER - IsFlaggedFraud
+    dataFrameStep5 = util.step_5(df)
+
+    X2 = dataFrameStep5[0]
+    Y2 = dataFrameStep5[1]
+    random_forest_model(X2, Y2)
 
 def decision_tree_model(depth, dfX, dfY):
     start_time = time.time()
@@ -140,7 +220,39 @@ def decision_tree_model(depth, dfX, dfY):
     end_time = time.time()
     print(' - - - - %s seconds - - - ' % (end_time-start_time))
 
-dataFrame = util.load_paysim_data()
-#comparing_step1(dataFrame)
+def random_forest_model(dfX, dfY):
+    start_time = time.time()
+    X_train, X_test, y_train, y_test = train_test_split(dfX, dfY, test_size=0.2, random_state=42,
+                                                        stratify=dfY)
 
-comparing_step5(dataFrame)
+    print('Transazioni Fraudolente TRAINING SET ', len(y_train.loc[(y_train == 1)]))
+    print('Transazioni Fraudolente TEST SET ', len(y_test.loc[(y_test == 1)]))
+
+    print('Transazioni NON Fraudolente TRAIN SET ', len(y_train.loc[(y_train == 0)]))
+    print('Transazioni NON Fraudolente TEST SET ', len(y_test.loc[(y_test == 0)]))
+
+    #randomForest = RandomForestClassifier(max_depth=depth)
+    randomForest = RandomForestClassifier()
+
+    randomForest.fit(X_train,y_train)
+
+    # Apply model to validation data
+    y_predictForest = randomForest.predict(X_test)
+
+    # Accuracy
+    print('Accuracy Score Random Forest: \n ', metrics.classification_report(y_test, y_predictForest))
+    print('Accuracy Score:', metrics.accuracy_score(y_test, y_predictForest))
+    confusion_matx = confusion_matrix(y_test, y_predictForest)
+    print(confusion_matx)
+    end_time = time.time()
+    print(' - - - - %s seconds - - - ' % (end_time - start_time))
+
+
+dataFrame = util.load_paysim_data()
+comparing_step1_randomforest(dataFrame)
+#comparing_step2_randomforest(dataFrame)
+#comparing_step3_randomforest(dataFrame)
+#comparing_step4_randomforest(dataFrame)
+#comparing_step5_randomforest(dataFrame)
+
+#comparing_step5(dataFrame)
